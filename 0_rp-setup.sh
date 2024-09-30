060c61106e4507ef36db886abdee2c956c136ec2
sabrinaboehm <sabrinaboehm911@gmx.de>
2024-09-30 14:13:53 +0200

    update 0_rp-setup.sh

--- 0_rp-setup.sh
+++ 0_rp-setup.sh
@@ -3,6 +3,7 @@ chmod +x 1_setup.sh
 chmod +x 2_run.sh
 chmod +x 3_plot.sh
 chmod +x evaluation.sh
+chmod +x gradlew
 
 cat results/2024-09-25_11-29-30/data/data-2024-09-26_10-55-02/part_* > results/2024-09-25_11-29-30/data/data-2024-09-26_10-55-02/partial_coverage.zip
 unzip results/2024-09-25_11-29-30/data/data-2024-09-26_10-55-02/partial_coverage.zip -d results/2024-09-25_11-29-30/data/data-2024-09-26_10-55-02/
